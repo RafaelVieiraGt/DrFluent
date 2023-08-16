59d49bbffccc0c85409f9b2573c58940a136aa25
Rafael Vieira <rafaelcamillivieira@gmail.com>
2023-08-16 11:24:25 -0300

    first update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,28 @@
 import './home.css';
 import Header from '../../Components/Header';
+import figura1 from '../../Assets/nuvemHome.JPG';
+import brilho from '../../Assets/BrilhoHome.JPG';
+
+import { Link } from 'react-router-dom';
 export default function Home(){
     return(
-        <div>
+        <div className='body-home'>
             <Header/>
+            <main>
+                <div className='conteudo-home'>
+                    <div className='txt-home'>
+                        <h1>Aprenda <br/>inglês de forma <br/>didática e divertida</h1>
+                        <span>Treine seu conhecimento com diversos temas atuais!</span>
+                        <Link to='/exercicios' className='btn-exercicios'>Começar teste</Link>
+                    </div>
+                    <div className='img-brilho'>
+                        <img src={brilho} alt='brilho'/>
+                    </div>
+                </div>
+                <div className='nuvem-img'>
+                    <img src={figura1} alt='figura de nuvem'/>
+                </div>
+            </main>
         </div>
     );
 }
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -10,7 +10,7 @@ export default function RoutesApp(){
         <Routes>
             <Route path="/" element={ <Login/> }/> 
             <Route path="/register" element={ <Register/> }/>
-            <Route path="/home" element={ <Home/> }/>
+            <Route path="/landing" element={ <Home/> }/>
         </Routes>
     );
 }
